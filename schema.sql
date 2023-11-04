24893b3a4b7614018c4d2459a4079635ba773f99
karlohrabar <karhill103@gmail.com>
2023-11-04 14:19:54 +0100

    database schema added

---
+++ schema.sql
@@ -0,0 +1,89 @@
+CREATE TABLE Users(
+	user_id int NOT NULL,
+	role varchar(20),
+	username varchar(30),
+	email varchar(30),
+	passwd varchar(60),
+	recipe_id int,
+	PRIMARY KEY (user_id),
+	FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id)
+);
+
+CREATE TABLE Recipe(
+	recipe_id int NOT NULL,
+	title varchar(30),
+	description text(max),
+	difficulty varchar(30),
+	prep_time smallint,
+	PRIMARY KEY (recipe_id)
+);
+
+CREATE TABLE Ingredient(
+	ingredient_id int NOT NULL,
+	ingredient_name varchar(40),
+	unit varchar(10),
+	PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE ShoppingList(
+  mealplan_id int UNIQUE NOT NULL,
+  shoppinglist_name varchar(30)
+);
+
+CREATE TABLE Review(
+	review_id int NOT NULL,
+	review_text text(max),
+	score tinyint,
+	review_date date,
+	recipe_id int,
+	user_id int,
+	PRIMARY KEY (review_id),
+	FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
+	
+);
+
+CREATE TABLE MealPlan(
+	mealplan_id int NOT NULL,
+	plan_name varchar(20),
+	description text(max),
+	plan_date date,	
+	user_id int,
+	PRIMARY KEY (mealplan_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
+	
+);
+
+CREATE TABLE MealPlanRecipe(
+	id int NOT NULL,
+	mealplan_id int,
+	recipe_id int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (mealplan_id) REFERENCES MealPlan(mealplan_id),
+	FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id)
+
+);
+
+CREATE TABLE RecipeIngredient(
+	id int NOT NULL,
+	recipe_id int,
+	ingredient_id int,
+	quantity int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id),
+	FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id)
+);
+
+CREATE TABLE ShoppingListIngredient(
+	id int NOT NULL,
+	shoppinglist_id int,
+	ingredient_id int,
+	quantity int,
+	unit varchar(10),
+	PRIMARY KEY (id),
+	FOREIGN KEY (shoppinglist_id) REFERENCES ShoppingList(mealplan_id),
+	FOREIGN KEY (ingredient_id) REFERENCES Ingredient (ingredient_id)
+);
+
+ALTER TABLE ShoppingList
+ADD CONSTRAINT mealplan_shoppinglist FOREIGN KEY (mealplan_id) REFERENCES MealPlan(mealplan_id);
