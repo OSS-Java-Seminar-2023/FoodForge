1bbda5044aad41c93e01a7027f56adf59f005d14
karlohrabar <karhill103@gmail.com>
2023-11-07 21:37:18 +0100

    project added, first db migration

---
+++ FoodForge/src/main/java/com/kuvari/FoodForge/FoodForgeApplication.java
@@ -0,0 +1,13 @@
+package com.kuvari.FoodForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodForgeApplication.class, args);
+	}
+
+}
---
+++ FoodForge/src/test/java/com/kuvari/FoodForge/FoodForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuvari.FoodForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
